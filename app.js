ba95e16d00c042aad73cceb53a3a3609832c16ca
khanh98hust <ngockhanh98tb@gmail.com>
2021-05-12 02:10:23 +0700

    add-back-to-top

--- app.js
+++ app.js
@@ -24,3 +24,18 @@ $(function () {
 
   var accordion = new Accordion($('#accordion'), false);
 });
+
+var btn = $('.on-top');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 900) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
